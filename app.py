63ca5ba4ae2ff7347edff3d2b4eea901a5acc679
BowlerHat83 <jamie@bowlerhat.co.uk>
2026-05-19 11:27:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ try:
     from scraper import get_landing_context
     from classify import classify_terms_batch
     from prefilter import contextual_prefilter
+    from intelli import contextual_prefilter
     from root import extract_roots
     from finalclass import final_classification
     from output import build_outputs
